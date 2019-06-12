7be68d82fc0c9f325182b5d9681ea7f59165b998
RifinTidHamar <42009503+RifinTidHamar@users.noreply.github.com>
2019-06-12 03:42:37 -0700

    added some comments

--- Skydive/Assets/Scripts/Player/Movement.cs
+++ Skydive/Assets/Scripts/Player/Movement.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+   //used for referencing the location of the knob
     public TrackMouse knob;
-    [SerializeField] Rigidbody player;
     float speedX;
     float speedY;
+
+   
     [SerializeField] float alt = 1;
 
     void Update()
     {
-
+        //used to determine when the finger has left the presmises of the circle
         float c = knob.c;
-        //Debug.Log(c);
         if (Input.touchCount == 1 || Input.GetKey(KeyCode.Mouse0))
         {
-            speedX = -knob.x / Screen.width * alt;
-            speedY = -knob.y / Screen.height * alt;
+            speedX = -knob.x * alt / Screen.width ;
+            speedY = -knob.y * alt / Screen.height;
+
+            //while inside the circle the speed is adjustable
             if (c < 175)
             {
                 transform.Translate(new Vector3(speedX, speedY));
             }
+
+            //while outside the circle the speed is not adjustable (velocity is)
             else
             {
-                transform.Translate(new Vector3(speedX, speedY).normalized * 0.13f * alt);
+                transform.Translate(new Vector3(speedX, speedY).normalized * 0.13f / alt);
             }
         }
-        //Debug.Log(speedY / speedX);
     }
 }
